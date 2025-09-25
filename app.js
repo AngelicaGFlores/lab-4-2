7a160bcbd37e2a27a4c5a42f2096e09ac60d6e7a
AngelicaGFlores <flores.guada.angelica@gmail.com>
2025-09-24 19:00:35 -0700

    resubmitting theory couldn't get why i get some errors :(

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-let shoppingList = [];
+const shoppingList = [];
 
 // TASK 1 complete
 
 function addItem(anyparam) {
-	shoppingList.push(anyparam);
+	if (!shoppingList.includes(anyparam)) shoppingList.push(anyparam);
 	return shoppingList;
 }
 
@@ -17,10 +17,15 @@ function removeLastItem() {
 	return shoppingList / pop();
 }
 //removeLastItem
-console.log(removeLastItem);
+//console.log(removeLastItem);
 
 // console.log(shoppingList.pop())
 console.log(shoppingList);
 
-
-
+function filterItems(searchTerm) {
+	//terrible way to do this
+	// return a new aaray that has the searchTerm
+	const newarray = shoppingList.filter((searchT) => searchT === searchTerm);
+	//just return the elements in the array
+	return newarray.forEach((element) => console.log(element));
+}
